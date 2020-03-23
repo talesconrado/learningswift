4a789d9f3023db44cf89797869ba50a5e43e1766
Tales Conrado <talesmateuscc@gmail.com>
2020-03-23 14:55:28 -0300

    Adiciona teste de memória em dict

--- NanoSwift/setDict.swift
+++ NanoSwift/setDict.swift
@@ -3,12 +3,12 @@
 
 var dictCores = [String: Set<String>]()
 
-for _ in 1...3{
+for _ in 1...2{
     print("Digite uma cor: ")
     let novaCor = readLine()
     var memorias: Set<String> = []
     if let novaCor = novaCor{
-        for _ in 1...4{
+        for _ in 1...2{
             print("Digite algo que te remete essa cor: ")
             let memoria = readLine()
             if let memoria = memoria{
@@ -19,6 +19,45 @@ for _ in 1...3{
     }
 }    
 
-print(dictCores)
+//Agora vamos testar se o usuário realmente lembra o q ele associou a cada cor (????) 
+//Estou sem muita criatividade :/
+enum Lembrando{
+    case pouco
+    case medio
+    case tudo
+}
 
-//Agora vamos testar se o usuário realmente lembra o q ele associou a cada cor (????) Estou sem muita criatividade
+var contador = 0
+
+for (cor, conjuntoDeMemorias) in dictCores{
+    print("Diga que cor isso te lembra: ")
+    for memoria in conjuntoDeMemorias{
+        print(memoria)
+    }
+    let resposta = readLine()
+    if let resposta = resposta, resposta == cor{
+        contador += 1
+    }
+    print("Próxima.")
+}
+
+var resultado:Lembrando
+
+if contador == 0 {
+    resultado = .pouco
+} else if contador==1 {
+    resultado = .medio
+} else {
+    resultado = .tudo
+}
+
+switch resultado {
+case .pouco:
+    print("Memória fraca.")
+case .medio:
+    print("Tá até ok.")
+case .tudo:
+    print("Boa.")
+}
+
+
